8a330775a0c2d36a1d5aa64932b522c84b73c353
Jzereael <jzereael@gmail.com>
2023-05-29 13:33:27 -0600

    Added additional server api

---
+++ first-api/routes/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// GET request
+router.get('/', function (req, res) {
+res.status(200).send('Server is up and running.')
+})
+// POST request
+router.post('/', function (req, res) {
+res.status(200).send('This is a POST request')
+})
+// PUT Request
+router.put('/', function (req, res) {
+res.status(200).send('This is a PUT request')
+})
+// PATCH Request
+router.patch('/', function (req, res) {
+res.status(200).send('This is a PATCH request')
+})
+// DELETE Request
+router.delete('/', function (req, res) {
+res.status(200).send('This is a DELETE request')
+})
+module.exports = router;
